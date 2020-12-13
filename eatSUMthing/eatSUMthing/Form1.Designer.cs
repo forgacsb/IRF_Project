fc3532a124eb262e82fd5dd82f8bab7fb18d9441
Benjámin Forgács <benjamin.forgacs@stud.uni-corvinus.hu>
2020-12-13 15:29:51 +0100

    UI desing

--- eatSUMthing/eatSUMthing/Form1.Designer.cs
+++ eatSUMthing/eatSUMthing/Form1.Designer.cs
@@ -41,7 +41,8 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -73,7 +74,7 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(192, 25);
+            this.comboBox2.Location = new System.Drawing.Point(637, 271);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(80, 21);
             this.comboBox2.TabIndex = 3;
@@ -81,7 +82,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(189, 9);
+            this.label2.Location = new System.Drawing.Point(634, 255);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(20, 13);
             this.label2.TabIndex = 4;
@@ -116,7 +117,7 @@
             // comboBox4
             // 
             this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(399, 25);
+            this.comboBox4.Location = new System.Drawing.Point(192, 25);
             this.comboBox4.Name = "comboBox4";
             this.comboBox4.Size = new System.Drawing.Size(232, 21);
             this.comboBox4.TabIndex = 8;
@@ -124,7 +125,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(396, 9);
+            this.label4.Location = new System.Drawing.Point(189, 9);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(55, 13);
             this.label4.TabIndex = 9;
@@ -141,7 +142,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(637, 164);
+            this.button3.Location = new System.Drawing.Point(637, 108);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(151, 50);
             this.button3.TabIndex = 11;
@@ -150,28 +151,37 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(637, 220);
+            this.button4.Location = new System.Drawing.Point(637, 164);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(151, 50);
             this.button4.TabIndex = 12;
             this.button4.Text = "Árkalkuláció";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // comboBox5
             // 
-            this.button5.Location = new System.Drawing.Point(637, 108);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(151, 50);
-            this.button5.TabIndex = 13;
-            this.button5.Text = "Kijelölt személy adatmódosítása";
-            this.button5.UseVisualStyleBackColor = true;
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(430, 25);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(80, 21);
+            this.comboBox5.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(427, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Osztály";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -208,7 +218,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.Label label5;
     }
 }
 
